e0d67769676fefe0778cc902540c8fab7d5707d3
FRAMEEE17 <frame.sk140@gmail.com>
2025-07-05 15:24:27 +0000

    remove large dir

--- bench/run_vllm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-vllm serve google/gemma-3-27b-it \
-    --enable-lora \
-    --lora-module lora=/home/siamai/llmtune/LLaMA-Factory/MonkeyReasonerExport \
-    --tensor-parallel-size 8 \
-    --max-model-len 8192 \
-    --port 2124
